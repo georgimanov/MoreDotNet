6269c70d491e4587a3703cb0918f3ce315d241b2
Georgi Manov <georgimanov@gmail.com>
2017-10-24 01:28:44 +0300

    added system time and firstDayOfMonthTests

---
+++ Source/MoreDotNet.Test/Extensions/Common/DateTimeExtensions/FirstDayOfMonthTests.cs
@@ -0,0 +1,58 @@
+﻿namespace MoreDotNet.Tests.Extensions.Common.DateTimeExtensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MoreDotNet.Extensions.Common;
+
+    using Xunit;
+
+    public class FirstDayOfMonthTests
+    {
+        public static IEnumerable<object[]> DateTimeList
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new DateTime(2017, 11, 1) },
+                    new object[] { new DateTime(2017, 11, 15) },
+                    new object[] { new DateTime(2017, 11, 30) },
+                };
+            }
+        }
+
+        public static IEnumerable<object[]> DateTimeWithDaysOfWeekList
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new DateTime(2017, 11, 1), DayOfWeek.Monday, new DateTime(2017, 11, 6) },
+                    new object[] { new DateTime(2017, 11, 15), DayOfWeek.Friday, new DateTime(2017, 11, 3) },
+                    new object[] { new DateTime(2017, 11, 30), DayOfWeek.Saturday, new DateTime(2017, 11, 4) },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(DateTimeList))]
+        public void FirstDayOfMonth_ShouldReturn_CorrectValue(DateTime currentDate)
+        {
+            SystemTime.SetDateTime(currentDate);
+            var firstDayOfMonthExpected = new DateTime(2017, 11, 1);
+
+            var firstDayOfMonthActual = currentDate.FirstDayOfMonth();
+            Assert.Equal(firstDayOfMonthExpected, firstDayOfMonthActual);
+        }
+
+        [Theory]
+        [MemberData(nameof(DateTimeWithDaysOfWeekList))]
+        public void FirstDayOfMonthWithSpecifiedDayOfWeek_ShouldReturn_CorrectValue(DateTime currentDate, DayOfWeek dayOfWeek, DateTime firstDayOfMonthExpected)
+        {
+            SystemTime.SetDateTime(currentDate);
+            var firstDayOfMonthActual = currentDate.FirstDayOfMonth(dayOfWeek);
+            Assert.Equal(firstDayOfMonthExpected, firstDayOfMonthActual);
+        }
+    }
+}
