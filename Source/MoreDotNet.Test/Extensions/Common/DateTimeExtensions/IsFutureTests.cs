7aca4944a395214812542853077c0eef60c40b84
Georgi Manov <georgimanov@gmail.com>
2017-12-06 08:25:20 +0200

    added Is Future Tests

---
+++ Source/MoreDotNet.Test/Extensions/Common/DateTimeExtensions/IsFutureTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit.Sdk;
+
+namespace MoreDotNet.Tests.Extensions.Common.DateTimeExtensions
+{
+    using System;
+    using MoreDotNet.Extensions.Common;
+    using Xunit;
+
+    public class IsFutureTests
+    {
+        [Fact]
+        public void IsFuture_ShouldReturn_False()
+        {
+            var checkDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day - 1);
+
+            Assert.False(checkDateTime.IsFuture());
+        }
+
+        [Fact]
+        public void IsFuture_ShouldReturn_True()
+        {
+            var checkDateTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day + 1);
+
+            Assert.True(checkDateTime.IsFuture());
+        }
+
+        [Fact(Skip = "Needs further investigation")]
+        public void IsFuture_ShouldReturn_True_ForDateTimeNowPlusOneHour_Local()
+        {
+            var now = DateTime.Now;
+            var checkDateTime = new DateTime(now.Year, now.Month, now.Day, now.Hour + 1, now.Minute, now.Second, DateTimeKind.Local);
+
+            Assert.Equal(now.Kind, checkDateTime.Kind);
+            Assert.True(checkDateTime.IsFuture());
+        }
+
+        [Fact]
+        public void IsFuture_ShouldReturn_False_ForDateTimeNowPlusOneHour_Utc()
+        {
+            var now = DateTime.Now;
+            var checkDateTime = new DateTime(now.Year, now.Month, now.Day, now.Hour + 1, now.Minute, now.Second, DateTimeKind.Utc);
+
+            Assert.NotEqual(now.Kind, checkDateTime.Kind);
+            Assert.False(checkDateTime.IsFuture());
+        }
+
+        [Fact]
+        public void IsFuture_ShouldReturn_False_ForDateTimeNowPlusOneHour_Unspecified()
+        {
+            var now = DateTime.Now;
+            var checkDateTime = new DateTime(now.Year, now.Month, now.Day, now.Hour + 1, now.Minute, now.Second, DateTimeKind.Unspecified);
+
+            Assert.NotEqual(now.Kind, checkDateTime.Kind);
+            Assert.False(checkDateTime.IsFuture());
+        }
+
+        [Fact]
+        public void IsFuture_ShouldReturn_Fasle_ForDateTimeNow()
+        {
+            Assert.False(DateTime.Now.IsFuture());
+        }
+    }
+}
--- Source/MoreDotNet.Test/Extensions/Common/DateTimeExtensions/WithTimeTests.cs
+++ Source/MoreDotNet.Test/Extensions/Common/DateTimeExtensions/WithTimeTests.cs
@@ -9,7 +9,7 @@
     public class WithTimeTests
     {
         [Fact]
-        public void WithTimeTests_ShouldReturn_CorrectValue()
+        public void WithTimeTest_ShouldReturn_CorrectValue()
         {
             const int iterations = 100;
             const int year = 2017;
@@ -19,19 +19,12 @@
             var random = new Random();
             for (var i = 0; i < iterations; i++)
             {
+                var currentDate = new DateTime(year, month, day);
+
                 var randomHours = random.Next(0, 24);
                 var randomMinutes = random.Next(0, 60);
                 var randomSeconds = random.Next(0, 60);
                 var randomMilliSeconds = random.Next(0, 1000);
-
-                var currentDate = new DateTime(year, month, day);
-                Assert.Equal(currentDate.Year, year);
-                Assert.Equal(currentDate.Month, month);
-                Assert.Equal(currentDate.Day, day);
-                Assert.Equal(currentDate.Hour, 0);
-                Assert.Equal(currentDate.Minute, 0);
-                Assert.Equal(currentDate.Second, 0);
-                Assert.Equal(currentDate.Millisecond, 0);
 
                 var resultDate = currentDate.WithTime(randomHours, randomMinutes, randomSeconds, randomMilliSeconds);
                 Assert.Equal(year, resultDate.Year);
